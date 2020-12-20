bbe6f52c67af19e7bd057b794734cad37f5e3f20
Jacob Toomey <jacobtoomey@pop-os.localdomain>
2020-12-20 10:04:19 -0800

    First commit

---
+++ api/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/html/index.html');
+});
+
+app.listen(port,  () =>{
+    console.log('Listening on $port');
+});
